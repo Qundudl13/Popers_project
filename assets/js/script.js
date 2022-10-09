c444453aa6d12dc76c8b12212ec3be7105e2ca94
Qundudl13 <yanovskiy.roma2017@gmail.com>
2022-10-09 14:38:30 +0300

    Добавление изменений

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,13 @@ $(document).ready(function() {
             $(this).children().slideToggle(500);
             $(this).toggleClass("header_bottom_nav_list_li_hover");
         })
+        for(let x of $(".header_bottom_nav_list_li_news_unit span")) {
+            var textNav = $(x).text().split('');
+            if(textNav.length > 19) {
+                var textNavLong = textNav.join('');
+                $(x).text(textNavLong.slice(0, 19) + "...");
+            }
+        }
     })
     $(function() {
         //buy buttons
@@ -15,8 +22,10 @@ $(document).ready(function() {
     $(function() {
         //answer buttons
         $(".content_main_faq_answer_btn_unit").click(function() {
-            $(this).siblings().slideToggle(500);
             $(this).toggleClass("content_main_faq_answer_btn_unit_hover");
+            $(this).parent().siblings().children().removeClass("content_main_faq_answer_btn_unit_hover");
+            $(".content_main_faq_answer_btn_unit").not(this).siblings().hide(500);
+            $(this).siblings().slideToggle(500);
         })
     })
 })
